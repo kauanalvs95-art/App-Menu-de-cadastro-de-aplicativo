9c31c3500325f018358fa15ff687f76003dd81ae
kauanalvs95-art <kauanalvs95@gmail.com>
2026-05-29 19:16:53 -0300

    Remove age classification logic from ex02.py

--- Sabor Express/ex02.py
+++ Sabor Express/ex02.py
@@ -1,7 +1,1 @@
-idade_usuario = int(input('Digite a sua idade: '))
-if idade_usuario <= 12:
-    print('Você é uma criança')
-elif idade_usuario <= 18:
-    print('Você é um adolescente')
-else:
-    print('Você é um adulto')
+
